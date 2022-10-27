e8d4be3b6cb205e192db01745932afe87dc4759c
HwiHwi523 <57697202+HwiHwi523@users.noreply.github.com>
2022-10-27 12:30:31 +0900

    Update settings.py
    docs: Django Secret Key 숨김

--- 1_API Server/Django/API_Django_WhatsOnMyPath/config/settings.py
+++ 1_API Server/Django/API_Django_WhatsOnMyPath/config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-mtsv_3&qd$1r4z9)y%jt#iac(&nzlc6+$!$@)8jbv&-@8srbw$"
+SECRET_KEY = "Django Secret Key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
