7166facecd8bec0046eb78df2776939dce88bc41
HwiHwi <yjh200523@naver.com>
2022-12-09 07:59:01 +0900

    init: REST API Spring 프로젝트 생성 및 초기 설정

---
+++ 1_API Server/WhatsOnMyPathSpring/src/test/java/com/yjh/whatsonmypath/WhatsOnMyPathSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjh.whatsonmypath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsOnMyPathSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
