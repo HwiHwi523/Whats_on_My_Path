fe7f83e34701bc9bfe425f507800e63eb3ece440
HwiHwi <yjh200523@naver.com>
2022-12-11 03:59:59 +0900

    model: 지점(경도, 위도)을 저장하는 데이터 모델

---
+++ 1_API Server/WhatsOnMyPathSpring/src/main/java/com/yjh/whatsonmypath/model/dto/Point.java
@@ -0,0 +1,11 @@
+package com.yjh.whatsonmypath.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Point {
+    String x;
+    String y;
+}
