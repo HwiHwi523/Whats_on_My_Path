dd88b4044c303d514266b37da8156dc42688f766
HwiHwi <yjh200523@naver.com>
2022-12-16 03:11:34 +0900

    style: Toggle, Mobile Navbar List JS 수정

--- 2_Web/WhatsOnMyPathVue/whats-on-my-path-vue/src/components/include/HeaderComponent.vue
+++ 2_Web/WhatsOnMyPathVue/whats-on-my-path-vue/src/components/include/HeaderComponent.vue
@@ -5,33 +5,32 @@
       class="container-fluid d-flex justify-content-between align-items-center"
     >
       <h1 class="logo me-auto me-lg-0">
-        <router-link :to="{ name: 'Index' }">What's on My Path ?</router-link>
+        <router-link :to="{ name: 'Index' }">WoMP</router-link>
       </h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
       <nav id="navbar" class="navbar order-last order-lg-0">
         <ul>
           <li>
-            <router-link class="active" :to="{ name: 'Index' }"
-              >Home
+            <router-link :to="{ name: 'Index' }" @click="moveRouter">
+              <h4 class="hangeul">홈으로</h4>
             </router-link>
           </li>
-          <li><router-link :to="{ name: 'About' }">About</router-link></li>
-          <li><a href="resume.html">Resume</a></li>
-          <li><a href="services.html">Services</a></li>
-          <li><a href="portfolio.html">Portfolio</a></li>
-          <li><a href="contact.html">Contact</a></li>
+          <li>
+            <router-link :to="{ name: 'About' }" @click="moveRouter">
+              <h4 class="hangeul">소개</h4>
+            </router-link>
+          </li>
+          <li>
+            <router-link :to="{ name: 'FindPath' }" @click="moveRouter">
+              <h4 class="hangeul">길찾기</h4>
+            </router-link>
+          </li>
         </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
+        <i class="bi bi-list mobile-nav-toggle" @click="clickMobileNavbar"></i>
       </nav>
       <!-- .navbar -->
 
       <div class="header-social-links">
-        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+        <a href="#" class="github"><i class="bi bi-github"></i></a>
       </div>
     </div>
   </header>
@@ -40,6 +39,27 @@
 
 <script>
 export default {
+  methods: {
+    clickMobileNavbar(event) {
+      document.querySelector("#navbar").classList.toggle("navbar-mobile");
+      event.target.classList.toggle("bi-list");
+      event.target.classList.toggle("bi-x");
+    },
+    moveRouter() {
+      // 모바일 내비바 끄기
+      let mobileNavbar = document.querySelector(".navbar-mobile");
+      if (mobileNavbar) {
+        mobileNavbar.classList.toggle("navbar-mobile");
+      }
+
+      // 모바일 내비바 버튼 토글 (O -> X, X -> O)
+      let mobileNavbarBtn = document.querySelector(".mobile-nav-toggle");
+      if (mobileNavbarBtn) {
+        mobileNavbarBtn.classList.toggle("bi-list");
+        mobileNavbarBtn.classList.toggle("bi-x");
+      }
+    },
+  },
   mounted() {
     /**
      * Easy selector helper function
@@ -80,30 +100,6 @@ export default {
         behavior: "smooth",
       });
     };
-
-    /**
-     * Mobile nav toggle
-     */
-    on("click", ".mobile-nav-toggle", function (e) {
-      select("#navbar").classList.toggle("navbar-mobile");
-      this.classList.toggle("bi-list");
-      this.classList.toggle("bi-x");
-    });
-
-    /**
-     * Mobile nav dropdowns activate
-     */
-    on(
-      "click",
-      ".navbar .dropdown > a",
-      function (e) {
-        if (select("#navbar").classList.contains("navbar-mobile")) {
-          e.preventDefault();
-          this.nextElementSibling.classList.toggle("dropdown-active");
-        }
-      },
-      true
-    );
 
     /**
      * Scroll with offset on links with a class name .scrollto
